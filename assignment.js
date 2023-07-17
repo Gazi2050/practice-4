44a2970a3ca8417a840873f30a5392273bff3fb6
Gazi2050 <gazi45344@gmail.com>
2023-07-17 22:05:44 +0600

    problem-2-solved

--- assignment.js
+++ assignment.js
@@ -14,3 +14,21 @@ let result = radianToDegree(radian)
 result = result.toFixed(2)
 result = parseFloat(result)
 console.log(result)
+
+/*(2)
+Function name : isJavaScriptFile
+cheak whether the given file name is javascript file or not
+*/
+function isJavaScriptFile(fileName) {
+    if (fileName.includes('.js.')) {
+        return 'Please enter a valid file name'
+    }
+    if (fileName.includes('.js')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+let nameOfFile = isJavaScriptFile('app.png');
+console.log(nameOfFile);
