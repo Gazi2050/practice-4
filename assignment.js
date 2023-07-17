9df3e1125d811b7897b7721eaac4cb1ed1fbb1c6
Gazi2050 <gazi45344@gmail.com>
2023-07-17 19:03:06 +0600

    problem-1-solved

--- assignment.js
+++ assignment.js
@@ -1,1 +1,16 @@
-//Assignment-JavaScript
+//Assignment-JavaScript//
+
+/*(1)
+Function name : radianToDegree
+input parameter : radian
+Output : degree
+*/
+function radianToDegree(radian) {
+    let degree = radian * (180 / Math.PI);
+    return degree;
+}
+let radian = 10;
+let result = radianToDegree(radian)
+result = result.toFixed(2)
+result = parseFloat(result)
+console.log(result)
