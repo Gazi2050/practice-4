983349318e4b21a170758f4b59580eb14bcc3844
Gazi2050 <gazi45344@gmail.com>
2023-07-18 06:35:14 +0600

    problem-3-solved

--- assignment.js
+++ assignment.js
@@ -17,7 +17,7 @@ console.log(result)
 
 /*(2)
 Function name : isJavaScriptFile
-cheak whether the given file name is javascript file or not
+check whether the given file name is javascript file or not
 */
 function isJavaScriptFile(fileName) {
     if (fileName.includes('.js.')) {
@@ -32,3 +32,24 @@ function isJavaScriptFile(fileName) {
 }
 let nameOfFile = isJavaScriptFile('app.png');
 console.log(nameOfFile);
+
+/*(3)
+Function name : oilPrice
+Parameter Count : 3
+diesel price 114TK/litre
+Petrol price 130TK/litre
+Octane price 135Tk/litre
+Output : calculate the total oil price that i have  to pay 
+*/
+function oilPrice(diesel, petrol, octane) {
+    const dieselPricePerLitre = 114;
+    const petrolPricePerLitre = 130;
+    const octanePricePerLitre = 135;
+    const dieselPrice = diesel * dieselPricePerLitre;
+    const petrolPrice = petrol * petrolPricePerLitre;
+    const octanePrice = octane * octanePricePerLitre;
+    const totalOilPrice = dieselPrice + petrolPrice + octanePrice;
+    return totalOilPrice;
+}
+let oils = oilPrice(2, 2, 2);
+console.log(oils);
